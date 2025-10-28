6af2ed7dcc00ce0c0beda08ec4b1cf63df9d1d00
sindhu834 <sindhuc424@gmail.com>
2025-10-28 19:00:35 +0530

    Time: 1 ms (38.6%), Space: 17.7 MB (78.68%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -2,9 +2,7 @@ class Solution:
     def scoreOfString(self, s: str) -> int:
         ans=0
         for i in range(len(s)-1):
-            a=ord(s[i])
-            b=ord(s[i+1])
-            temp= abs(a-b)
+            temp= abs(ord(s[i])-ord(s[i+1]))
             ans= ans + temp
         
         return ans

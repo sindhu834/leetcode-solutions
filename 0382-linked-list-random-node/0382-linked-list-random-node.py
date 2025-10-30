c40ba7370a2f226f65ae0c3e2ffcb95e1b7c39cf
sindhu834 <sindhuc424@gmail.com>
2025-10-30 18:27:58 +0530

    Time: 11 ms (80.59%), Space: 20.5 MB (73.63%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.py
@@ -0,0 +1,11 @@
+import random
+
+class Solution:
+    def __init__(self, head: Optional[ListNode]):
+        self.values = []
+        while head:
+            self.values.append(head.val)
+            head = head.next
+
+    def getRandom(self) -> int:
+        return random.choice(self.values)

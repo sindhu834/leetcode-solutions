9b1767757ab608376755d3fbd406543659f3eee7
sindhu834 <sindhuc424@gmail.com>
2026-01-22 21:21:06 +0530

    Time: 43 ms (33.22%), Space: 24.6 MB (64.85%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        
+        rows, cols = len(matrix), len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+            
+            # Calculate largest rectangle in histogram
+            max_area = max(max_area, self.largestRectangleArea(heights))
+        
+        return max_area
+    
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = []
+        max_area = 0
+        heights.append(0)  # sentinel
+        
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            stack.append(i)
+        
+        return max_area

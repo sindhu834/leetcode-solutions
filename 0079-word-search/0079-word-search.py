2b86c109d864b38c81f6a00a9e0a42b738f36a6f
sindhu834 <sindhuc424@gmail.com>
2026-01-30 21:07:08 +0530

    Time: 3152 ms (82.74%), Space: 19.5 MB (25.32%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,24 @@
-from typing import List
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-
-        def dfs(i, j, index):
-            # Check if all letters matched
-            if index == len(word):
-                return True
-            # Check boundaries and current letter
-            if i < 0 or i >= rows or j < 0 or j >= cols or board[i][j] != word[index]:
-                return False
-
-            # Mark the cell as visited
-            temp = board[i][j]
-            board[i][j] = '#'
-
-            # Explore 4 directions
-            found = (dfs(i+1, j, index+1) or
-                     dfs(i-1, j, index+1) or
-                     dfs(i, j+1, index+1) or
-                     dfs(i, j-1, index+1))
-
-            # Restore the cell
-            board[i][j] = temp
-            return found
-
-        # Start DFS from each cell
-        for i in range(rows):
-            for j in range(cols):
-                if dfs(i, j, 0):
-                    return True
+        def backtrack(row,col,board,word,ind):
+            if(ind == len(word)):
+                return True 
+            for r,c in [[-1,0],[1,0],[0,-1],[0,1]]:
+                nr = r+row 
+                nc = c+col 
+                if(nr>=0 and nr<n and nc>=0 and nc<m and board[nr][nc] == word[ind]):
+                    board[nr][nc] = '.'
+                    if(backtrack(nr,nc,board,word,ind+1)):
+                        return True 
+                    board[nr][nc] = word[ind] 
+            return False 
+        n = len(board) # no of rows 
+        m = len(board[0]) # no of cols
+        for row in range(0,n):
+            for col in range(0,m):
+                if(board[row][col] == word[0]): 
+                    board[row][col] = "." 
+                    if(backtrack(row,col,board,word,1)):
+                        return True 
+                    board[row][col] = word[0]
         return False

602b750e16b6e797642dceab0d1ef7875d97fb09
sindhu834 <sindhuc424@gmail.com>
2026-02-25 21:23:28 +0530

    Time: 11 ms (20.66%), Space: 19.4 MB (44.54%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortByBits(self, arr):
+        
+        def countBits(n):
+            count = 0
+            while n:
+                count += n & 1
+                n >>= 1
+            return count
+        
+        return sorted(arr, key=lambda x: (countBits(x), x))
